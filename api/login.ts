6642da49fa8e0c2852ecfe890321d4ddee0e118e
Henning Hall <henning.hall@hotmail.com>
2022-08-27 23:46:27 +0200

    require

--- api/login.ts
+++ api/login.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 const clientId = "Iv1.395428b4814a0264";
 
 interface LoginResponse {
