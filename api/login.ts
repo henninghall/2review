c4179ca001165e22010a01f1b9bbe05857e48f67
Henning Hall <henning.hall@hotmail.com>
2022-08-28 16:16:01 +0200

    add back cors

--- api/login.ts
+++ api/login.ts
@@ -43,7 +43,7 @@ async function handler(req: VercelRequest, res: VercelResponse) {
     return res.status(400).json({ error: e.message });
   }
 }
-export default handler;
+export default withCors(handler);
 
 const login = async ({ state, code, redirect_uri, type }: Body) => {
   const client_secret =
