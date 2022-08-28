c5fb950d85cc1934d6f201c27b81d48689614808
Henning Hall <henning.hall@hotmail.com>
2022-08-28 19:21:47 +0200

    settings conditional

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useToken } from "./auth/useToken";
 import { useShowHelp } from "./settings/useShowHelp";
 import { useShowInstall } from "./settings/useShowInstall";
 import { useShowSettings } from "./settings/useShowSettings";
@@ -11,13 +12,14 @@ export const Footer = () => {
   const [, setShowHelp] = useShowHelp();
   const [, setShowInstall] = useShowInstall();
   const [, setShowSettings] = useShowSettings();
+  const [token] = useToken();
 
   return (
     <Container>
       <A onClick={() => setShowHelp(true)}>Why don't all PR's show up?</A>
       <A onClick={() => setShowInstall(true)}>Installation</A>
       <A href="https://github.com/henninghall/2review">Github repo</A>
-      <A onClick={() => setShowSettings(true)}>Settings</A>
+      {token && <A onClick={() => setShowSettings(true)}>Settings</A>}
     </Container>
   );
 };
